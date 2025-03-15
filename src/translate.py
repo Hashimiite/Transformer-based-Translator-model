6c1cb8468b0e1c2d6dfe7ffec165055172aa1128
Hashimiite <hashimabdigani2@gmail.com>
2025-03-15 12:45:00 -0400

    Update translate.py

--- src/translate.py
+++ src/translate.py
@@ -7,11 +7,9 @@ class Translator:
         self.preprocessor = preprocessor
     
     def translate(self, sentence: str) -> str:
-        # Get input sequence
         input_tokens, _ = self.preprocessor.encode(sentence, "")
         encoder_input = tf.expand_dims(input_tokens, 0)
         
-        # Start with START token (vocab_size from preprocessor)
         decoder_input = tf.constant([self.preprocessor.vocab_size], dtype=tf.int32)
         output = tf.expand_dims(decoder_input, 0)
         
@@ -20,14 +18,11 @@ class Translator:
             predictions = predictions[:, -1:, :]
             predicted_id = tf.cast(tf.argmax(predictions, axis=-1), dtype=tf.int32)
             
-            # If we predicted the END token, break
             if int(predicted_id[0][0]) == self.preprocessor.vocab_size + 1:
                 break
                 
-            # Ensure types match for concatenation
             output = tf.concat([output, predicted_id], axis=-1)
         
-        # Convert output sequence to list of integers for decoding
         output_sequence = [int(i) for i in output[0].numpy() if int(i) < self.preprocessor.vocab_size]
         text = self.preprocessor.decode(output_sequence)
         
